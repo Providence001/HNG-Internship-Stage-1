42b1e0b5230163aa0efbc373848124704a7244b1
Providence001 <emezie.dike1@gmail.com>
2023-09-10 16:06:35 +0100

    my first HNG task

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const displayDay = document.querySelector('[data-testid = "currentDayOfTheWeek"]');
+const displayTime = document.querySelector('[data-testid = "currentUTCTime"]');
+
+const updateTime = ()=>{
+  const date = new Date();
+  const daysOfTheWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+]
+
+const dayNum = date.getUTCDay();
+const dayName = daysOfTheWeek[dayNum];
+
+displayDay.textContent = dayName;
+displayTime.textContent =date.getTime();
+}
+
+setInterval(updateTime,10
+)
